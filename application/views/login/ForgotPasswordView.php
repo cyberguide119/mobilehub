27ed23e6752a36555b0a95fff43a1d2511636277
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2013-12-03 19:52:43 +0530

    Revamped the forgot password screen

--- application/views/login/ForgotPasswordView.php
+++ application/views/login/ForgotPasswordView.php
@@ -6,22 +6,30 @@ and open the template in the editor.
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Forgot Password</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?php echo site_url('../resources/css/bootstrap.min.css') ?>" rel="stylesheet">
+        <link href="<?php echo site_url('../resources/css/site-theme.css') ?>" rel="stylesheet">
     </head>
     <body>
-        <h3>Reset Password</h3>
-        <p>If you are unable to access your account, we will send you a password reset link to your recovery email.</p>
-        <form method="POST" action="/MobileHub/index.php/login/auth/sendResetLink">
-            <table>
-                <tr>
-                   <td><input type="text" name='email' length="10" size="30" placeholder="Email address"></td>
-                </tr>
-                <tr>
-                   <td align="right"><input type="submit" value='Login'></td>
-                </tr>
-            </table>
-        </form>
-        
-        <?php echo $errmsg;?>
+        <div class="container">
+            <form method="POST" action="<?php echo site_url('auth/sendResetLink')?>" role="form" class="form-signup">
+                <h3>Reset Password</h3>
+                <p>If you are unable to access your account, we will send you a password reset link to your recovery email.</p>
+                <br>
+                <div class="form-group">
+                    <input type="text" class="form-control" name='email' length="10" size="30" placeholder="Email address">
+                </div>
+                <br>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-large btn-info">Send Link</button>
+                </div>
+                <?php 
+                    if ($errmsg != NULL){
+                        echo '<div class="alert alert-danger">' . $errmsg . '</div>';
+                    }
+                ?>
+            </form>
+        </div>
     </body>
 </html>
