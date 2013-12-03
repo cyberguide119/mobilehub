6e153b156e17f293a8279cc89f24230159e0423b
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2013-12-03 19:28:58 +0530

    More changes to the UI of the Registration form

--- application/views/login/RegisterView.php
+++ application/views/login/RegisterView.php
@@ -13,50 +13,54 @@ and open the template in the editor.
     </head>
     <body>
     <div class="container">
-            <h2>Create Account</h2>
-            <form action="<?php echo site_url('/auth/createaccount') ?>" method="POST" class="form-horizontal">
+            <form action="<?php echo site_url('/auth/createaccount') ?>" method="POST" class="form-horizontal form-signup">
+                <h2>Create Account</h2><br>
                 <div class="form-group">
-                    <label class="control-label col-sm-2">Username</label>
-                    <div class="col-sm-10">
+                    <label class="control-label col-sm-4">Username</label>
+                    <div class="col-sm-8">
                         <input type="text" class="form-control" name='uname' length="10" size="10" placeholder="Choose a display name (max 10 characters)">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-2">Full Name</label>
-                    <div class="col-sm-10">
+                    <label class="control-label col-sm-4">Full Name</label>
+                    <div class="col-sm-8">
                         <input type="text" class="form-control" name='name' length="20" size="50" placeholder="Your first name and last name (Optional)">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-2">Website</label>
-                    <div class="col-sm-10">
+                    <label class="control-label col-sm-4">Website</label>
+                    <div class="col-sm-8">
                         <input type="text" class="form-control" name='website' length="50" size="50" placeholder="URL of your personal blog or website">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-2">Account password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control" name='pword' length="15" size="30" placeholder="Password">
+                    <label class="control-label col-sm-4">Password</label>
+                    <div class="col-sm-8">
+                        <input type="password" class="form-control" name='pword' length="15" size="30" placeholder="Account Password">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-2">Confirm password</label>
-                    <div class="col-sm-10">
+                    <label class="control-label col-sm-4">Password (Confirm)</label>
+                    <div class="col-sm-8">
                         <input type="password" class="form-control" name='conf_pword' length="15" size="30" placeholder="Please retype password">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-2">Email</label>
-                    <div class="col-sm-10">
+                    <label class="control-label col-sm-4">Email</label>
+                    <div class="col-sm-8">
                         <input type="text" class="form-control" name='email' length="50" size="50" placeholder="Please enter your email">
                     </div>
                 </div>
                 <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10">
+                    <div class="col-sm-offset-4 col-sm-8">
                         <button type="submit" class="btn btn-success">Register</button>
                     </div>
                 </div>
-        <span style="color: red"><?php echo $errmsg ?></span> <br>
+                <?php 
+                    if ($errmsg != NULL){
+                        echo '<div class="alert alert-danger">' . $errmsg . '</div>';
+                    }
+                ?>
         </form>
     </div>
     <script src="https://code.jquery.com/jquery.js"></script>
