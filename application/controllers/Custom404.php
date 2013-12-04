5443017a54814fa46ddff6ce2b9d4fb20fbfc528
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2013-12-05 02:25:58 +0530

    Custom made 404 page was added

---
+++ application/controllers/Custom404.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Custom404
+ *
+ * @author DRX
+ */
+class Custom404 extends CI_Controller 
+{
+    public function __construct()   {
+            parent::__construct();  
+    }
+    public function index() 
+    {
+        $this->output->set_status_header('404');
+        $data['content'] = 'errors/Error404'; // View name
+        $this->load->view('errors/Error404',$data);//loading in my template
+    }
+}
+
+?>
---
+++ application/views/errors/Error404.php
@@ -0,0 +1,37 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Error 404</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?php echo site_url('../resources/css/bootstrap.min.css') ?>" rel="stylesheet">
+        <link href="<?php echo site_url('../resources/css/site-theme.css') ?>" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="error-template">
+                        <h1>
+                            Oops!</h1>
+                        <h2>
+                            404 Not Found</h2>
+                        <div class="error-details">
+                            Sorry, an error has occured, Requested page not found!
+                        </div>
+                        <div class="error-actions">
+                            <a href="<?php echo site_url() ?>" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
+                                Take Me Home </a><a href="<?php echo site_url() ?>" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span> Contact Support </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <script src="<?php echo site_url('../resources/js/bootstrap.min.js')?>"></script>
+    </body>
+</html>
