c565338a275fc19c9b1badc6c4226098a78d110a
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2013-12-29 02:19:21 +0530

    simple captcha

--- application/views/login/RegisterView.php
+++ application/views/login/RegisterView.php
@@ -6,7 +6,7 @@
     </ul>
     <div class="tab-content">
         <div class="tab-pane active" id="student">
-            <form action="<?php echo site_url('api/auth/create') ?>" method="POST" class="form-horizontal form-signup" id="registerFormStudent">
+            <form action="<?php echo site_url('api/auth/create') ?>" method="POST" class="form-horizontal form-signup" id="registerFormStudent" onsubmit="return checkform(this);">
                 <legend>Create Account</legend><br>
                 <div class="form-group">
                     <label class="control-label col-sm-4">Username</label>
@@ -47,6 +47,11 @@
                 </div>
                 <div class="form-group">
                     <div class="col-sm-offset-4 col-sm-8">
+                        <p>
+                            <label for="code">Write code below > <span id="txtCaptchaDiv" style="color:#F00"></span><!-- this is where the script will place the generated code --> 
+                                <input type="hidden" id="txtCaptcha" /></label><!-- this is where the script will place a copy of the code for validation: this is a hidden field -->
+                            <input type="text" name="txtInput" id="txtInput" size="30" />
+                        </p>
                         <button type="submit" class="btn btn-success">Register</button>
                         <button type="reset" class="btn btn-large btn-default">Reset</button>
                     </div>
@@ -55,7 +60,7 @@
             </form>
         </div>
         <div class="tab-pane" id="tutor">
-            <form action="<?php echo site_url('api/auth/create') ?>" method="POST" class="form-horizontal form-signup" id="registerFormTutor">
+            <form action="<?php echo site_url('api/auth/create') ?>" method="POST" class="form-horizontal form-signup" id="registerFormTutor" onsubmit="return checkform(this);">
                 <legend>Create Account</legend><br>
                 <div class="form-group">
                     <label class="control-label col-sm-4">Username</label>
@@ -108,6 +113,11 @@
                 </div>
                 <div class="form-group">
                     <div class="col-sm-offset-4 col-sm-8">
+                        <p>
+                            <label for="code">Write code below > <span id="txtCaptchaDiv2" style="color:#F00"></span><!-- this is where the script will place the generated code --> 
+                                <input type="hidden" id="txtCaptcha2" /></label><!-- this is where the script will place a copy of the code for validation: this is a hidden field -->
+                            <input type="text" name="txtInput" id="txtInput" size="30" />
+                        </p>
                         <button type="submit" class="btn btn-success">Request for Tutor profile</button>
                         <button type="reset" class="btn btn-large btn-default">Reset</button>
                     </div>
@@ -150,75 +160,127 @@
     </div><!-- /.modal -->
 </div>
 <script>
-                        /* important to locate this script AFTER the closing form element, so form object is loaded in DOM before setup is called */
-                        $.fn.serializeObject = function()
-                        {
-                            var o = {};
-                            var a = this.serializeArray();
-                            $.each(a, function() {
-                                if (o[this.name] !== undefined) {
-                                    if (!o[this.name].push) {
-                                        o[this.name] = [o[this.name]];
-                                    }
-                                    o[this.name].push(this.value || '');
-                                } else {
-                                    o[this.name] = this.value || '';
-                                }
-                            });
-                            return o;
+                /* important to locate this script AFTER the closing form element, so form object is loaded in DOM before setup is called */
+                $.fn.serializeObject = function()
+                {
+                    var o = {};
+                    var a = this.serializeArray();
+                    $.each(a, function() {
+                        if (o[this.name] !== undefined) {
+                            if (!o[this.name].push) {
+                                o[this.name] = [o[this.name]];
+                            }
+                            o[this.name].push(this.value || '');
+                        } else {
+                            o[this.name] = this.value || '';
+                        }
+                    });
+                    return o;
+                };
+
+                $.validate({
+                    modules: 'date, security',
+                    onModulesLoaded: function() {
+                        var optionalConfig = {
+                            fontSize: '10pt',
+                            padding: '4px',
+                            bad: 'Very bad',
+                            weak: 'Weak',
+                            good: 'Good',
+                            strong: 'Strong'
                         };
 
-                        $.validate({
-                            modules: 'date, security',
-                            onModulesLoaded: function() {
-                                var optionalConfig = {
-                                    fontSize: '10pt',
-                                    padding: '4px',
-                                    bad: 'Very bad',
-                                    weak: 'Weak',
-                                    good: 'Good',
-                                    strong: 'Strong'
-                                };
+                        $('input[name="pword_confirmation"]').displayPasswordStrength(optionalConfig);
+                    },
+                    onSuccess: function() {
+                        $dataToSend = new Array();
+                        var activeTab = $("ul#regTabs li.active");
 
-                                $('input[name="pword_confirmation"]').displayPasswordStrength(optionalConfig);
-                            },
-                            onSuccess: function() {
-                                $dataToSend = new Array();
-                                var activeTab = $("ul#regTabs li.active");
+                        isTutor = (activeTab[0].innerHTML).indexOf("Tutor") !== -1;
 
-                                isTutor = (activeTab[0].innerHTML).indexOf("Tutor") !== -1;
-
-                                if (isTutor) {
-                                    $registerForm = $("#registerFormTutor");
-                                } else {
-                                    $registerForm = $("#registerFormStudent");
-                                }
-                                $serializedData = $registerForm.serializeObject();
-
-                                if (isTutor) {
-                                    $serializedData['isTutor'] = true;
-                                } else {
-                                    $serializedData['isTutor'] = false;
-                                }
-
-                                console.log($serializedData);
-                                $.post("/MobileHub/index.php/api/auth/create", $serializedData, function(content) {
-
-                                    // Deserialise the JSON
-                                    content = jQuery.parseJSON(content);
-                                    if (content.message === "Success") {
-                                        $('#succModalBody').html("<p><center>" + content.type + "</center></p>");
-                                        $('#succModal').modal('show');
-                                    } else {
-                                        $('#errModalBody').html("<p><center>" + content.type + "</center></p>");
-                                        $('#errorModal').modal('show');
-                                    }
-                                }), "json";
-                                return true;
-                            }
-                        });
-
-                        function reload() {
-                            window.location = '/MobileHub/';
+                        if (isTutor) {
+                            $registerForm = $("#registerFormTutor");
+                        } else {
+                            $registerForm = $("#registerFormStudent");
                         }
+                        $serializedData = $registerForm.serializeObject();
+
+                        if (isTutor) {
+                            $serializedData['isTutor'] = true;
+                        } else {
+                            $serializedData['isTutor'] = false;
+                        }
+
+                        console.log($serializedData);
+                        $.post("/MobileHub/index.php/api/auth/create", $serializedData, function(content) {
+
+                            // Deserialise the JSON
+                            content = jQuery.parseJSON(content);
+                            if (content.message === "Success") {
+                                $('#succModalBody').html("<p><center>" + content.type + "</center></p>");
+                                $('#succModal').modal('show');
+                            } else {
+                                $('#errModalBody').html("<p><center>" + content.type + "</center></p>");
+                                $('#errorModal').modal('show');
+                            }
+                        }), "json";
+                        return true;
+                    }
+                });
+
+                function reload() {
+                    window.location = '/MobileHub/';
+                }
+
+
+</script>
+
+<script type="text/javascript">
+
+    //Generates the captcha function    
+    var a = Math.ceil(Math.random() * 9) + '';
+    var b = Math.ceil(Math.random() * 9) + '';
+    var c = Math.ceil(Math.random() * 9) + '';
+    var d = Math.ceil(Math.random() * 9) + '';
+    var e = Math.ceil(Math.random() * 9) + '';
+
+    var code = a + b + c + d + e;
+    document.getElementById("txtCaptcha").value = code;
+    document.getElementById("txtCaptcha2").value = code;
+    document.getElementById("txtCaptchaDiv").innerHTML = code;
+    document.getElementById("txtCaptchaDiv2").innerHTML = code;
+
+    function checkform(theform) {
+        var why = "";
+
+        if (theform.txtInput.value === "") {
+            why += "- Security code should not be empty.\n";
+        }
+        if (theform.txtInput.value !== "") {
+            if (ValidCaptcha(theform.txtInput.value) === false) {
+                why += "- Security code did not match.\n";
+            }
+        }
+        if (why !== "") {
+            $('#errModalBody').html("<p><center>" + why + "</center></p>");
+            $('#errorModal').modal('show');
+            return false;
+        }
+    }
+
+// Validate the Entered input aganist the generated security code function   
+    function ValidCaptcha() {
+        var str1 = removeSpaces(document.getElementById('txtCaptcha').value);
+        var str2 = removeSpaces(document.getElementById('txtInput').value);
+        if (str1 === str2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+// Remove the spaces from the entered and generated code
+    function removeSpaces(string) {
+        return string.split(' ').join('');
+    }
 </script>
